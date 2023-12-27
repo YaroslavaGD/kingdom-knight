a6ef35781877129c58a161616619a0643788f5d1
Yaroslava Hryzadubova <yaroslava.gd@gmail.com>
2023-12-27 16:31:04 +0100

    feat: add camera and chests

--- src/scenes/level-1/index.ts
+++ src/scenes/level-1/index.ts
@@ -1,14 +1,14 @@
 import { Scene, Tilemaps } from "phaser";
 import { Player } from "../../classes/player";
+import { gameObjectsToObjectPoints } from "../../helpers/gameobject-to-objectpoint";
 
 export class Level1 extends Scene {
   private player!: Player;
-
   private map!: Tilemaps.Tilemap;
   private tileset!: Tilemaps.Tileset | null;
   private wallsLayer!: Tilemaps.TilemapLayer | null;
   private groundLayer!: Tilemaps.TilemapLayer | null;
-
+  private chests!: Phaser.GameObjects.Sprite[];
 
   constructor() {
     super('level-1-scene');
@@ -16,6 +16,8 @@ export class Level1 extends Scene {
   create(): void {
     this.initMap();
     this.player = new Player(this, 100, 100);
+    this.initChests();
+    this.initCamera();
     if (this.wallsLayer) {
       this.physics.add.collider(this.player, this.wallsLayer);
     }
@@ -55,5 +57,26 @@ export class Level1 extends Scene {
       tileColor: null,
       collidingTileColor: new Phaser.Display.Color(243, 234, 48, 255)
     });
+  }
+
+  private initChests(): void {
+    const chestObjects = this.map.filterObjects('Chests', obj => obj.name === 'ChestPoint');
+    if (chestObjects !== null) {
+      const chestPoints = gameObjectsToObjectPoints(chestObjects);
+      this.chests = chestPoints.map(chestPoint => this.physics.add.sprite(chestPoint.x, chestPoint.y, 'tiles_spr', 595).setScale(1.5));
+
+      this.chests.forEach(chest => {
+        this.physics.add.overlap(this.player, chest, (obj1, obj2) => {
+          obj2.destroy();
+          this.cameras.main.flash();
+        });
+      });
+    }
+  }
+
+  private initCamera(): void {
+    this.cameras.main.setSize(this.game.scale.width, this.game.scale.height);
+    this.cameras.main.startFollow(this.player, true, 0.09, 0.09);
+    this.cameras.main.setZoom(2);
   }
 }
--- src/scenes/loading/index.ts
+++ src/scenes/loading/index.ts
@@ -8,14 +8,23 @@ export class LoadingScene extends Scene {
 
   preload(): void {
     this.load.baseURL = 'assets/';
+
+    //PLAYER LOADING
     this.load.image('king', 'sprites/king.png');
     this.load.atlas('a-king', 'spritesheets/a-king.png', 'spritesheets/a-king_atlas.json');
     
+    //MAP LOADING
     this.load.image({
       key: 'tiles',
       url: 'tilemaps/tiles/dungeon-16-16.png',
     });
     this.load.tilemapTiledJSON('dungeon', 'tilemaps/json/dungeon.json');
+
+    //CHEST LOADING
+    this.load.spritesheet('tiles_spr', 'tilemaps/tiles/dungeon-16-16.png', {
+      frameWidth: 16,
+      frameHeight: 16,
+    })
   }
 
   create(): void {
